32234f5a5f42951228348fcb9087ef2e93d6d267
Juan Paz <jpaz.dev94@gmail.com>
2023-11-01 12:22:17 -0300

    Subiendo repaso clase 2

---
+++ clase-2/repaso-clase-2/test/src/components/Header.jsx
@@ -0,0 +1,16 @@
+function Header() {
+    return (
+        <header>
+            <h1>Aplicacion de prueba</h1>
+            <nav>
+                <ul>
+                    <li><a>Seccion 1</a></li>
+                    <li><a>Seccion 2</a></li>
+                    <li><a>Seccion 3</a></li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default Header;
---
+++ clase-2/repaso-clase-2/test/src/components/SearchBar.jsx
@@ -0,0 +1,10 @@
+function SearchBar() {
+    return (
+            <form>
+                <input type="search" placeholder="Buscar..." />
+            </form>
+    );
+}
+
+/*Por el momento no importo el search bar porque sino se rompe todo*/
+export default SearchBar();
